106c45f34db4af3f1c56a714e2c733695f8fbe80
front-endapps <website_dev@yahoo.com>
2024-12-06 19:28:59 +0200

    commit

--- copy-files.js
+++ copy-files.js
@@ -3,8 +3,12 @@ import path from 'path';
 
 const indexPath = path.join(process.cwd(), 'dist', 'index.html');
 
+// Verifică dacă fișierul există înainte de a-l modifica
 async function updateIndexHtml() {
   try {
+    // Verifică dacă fișierul există
+    await fs.access(indexPath);
+
     const data = await fs.readFile(indexPath, 'utf8');
 
     const updatedData = data.replace(
@@ -15,7 +19,11 @@ async function updateIndexHtml() {
     await fs.writeFile(indexPath, updatedData, 'utf8');
     console.log('Fișierul index.html a fost actualizat cu succes!');
   } catch (err) {
-    console.error('Eroare la procesarea fișierului index.html:', err);
+    if (err.code === 'ENOENT') {
+      console.error('Fișierul index.html nu a fost găsit în dist!');
+    } else {
+      console.error('Eroare la procesarea fișierului index.html:', err);
+    }
   }
 }
 
